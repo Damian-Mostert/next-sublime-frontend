98be68b65bca2ac63b99f335243ca87432513929
Damian Mostert <damianmostert86@gmail.com>
2024-04-24 19:44:50 +0200

    fix

--- src/app/api/clear-cache/route.js
+++ src/app/api/clear-cache/route.js
@@ -1,14 +1,5 @@
 import { NextResponse } from "next/server";
 
-import sections from "@app/components/__load";
-
 export function GET() {
-  return NextResponse.json(
-    Object.keys(sections).map((key) => {
-      return {
-        key,
-        configuration: sections[key].configuration,
-      };
-    })
-  );
+  return NextResponse.json({});
 }
